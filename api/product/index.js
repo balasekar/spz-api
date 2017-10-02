f5fbc08cdc04c7bdb3bcd5a40b043705e7998f43
balasekar <baalasekar@gmail.com>
2017-10-02 22:42:53 +0100

    mongo for products

--- api/product/index.js
+++ api/product/index.js
@@ -1,9 +1,17 @@
 const products = require('./products.json');
-
+const connection = require('../../mongoDB').getConnection();
 /** GET users listing. */
 module.exports = {
     getProducts(req, res) {
-        res.json(products);
+        // res.json(products);
+        connection.collection('products').find({}).toArray((err, docs) => {
+            if (err) {
+                console.log(err);
+                res.error(err);
+            } else {
+                res.json(docs);
+            }
+        });
         return res;
     }
 };
