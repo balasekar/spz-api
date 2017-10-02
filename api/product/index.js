023077fa3d2a06531db7628b8b58fc8375149c80
balasekar <baalasekar@gmail.com>
2017-10-02 22:51:22 +0100

    fix mongo connection

--- api/product/index.js
+++ api/product/index.js
@@ -1,17 +1,17 @@
-const products = require('./products.json');
-const connection = require('../../mongoDB').getConnection();
+// const products = require('./products.json');
+const mongoDB = require('../../mongoDB');
 /** GET users listing. */
 module.exports = {
     getProducts(req, res) {
         // res.json(products);
-        connection.collection('products').find({}).toArray((err, docs) => {
+        mongoDB.connect();
+        mongoDB.getConnection().collection('products').find({}).toArray((err, docs) => {
             if (err) {
                 console.log(err);
                 res.error(err);
             } else {
                 res.json(docs);
             }
-        });
-        return res;
+        }).then(() => res);
     }
 };
